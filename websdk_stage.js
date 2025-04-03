0f27b18fd84493d3ceb0ecba8e99945c936490f6
RevanthDigitalInterakt <revanth@rezultize.com>
2025-04-03 18:46:02 +0530

    login with pass fix

--- websdk_stage.js
+++ websdk_stage.js
@@ -462,9 +462,51 @@ document.addEventListener("DOMContentLoaded", function () {
                 }),                
 
                //login with password
-                listener("click","continueBtn",(event)=>{
-                    console.log("entered login with password");
+                // listener("click","continueBtn",(event)=>{
+                //     console.log("entered login with password");
+                //     event.preventDefault();
 
+                //     let phoneNumber = document.querySelector("#phone").value.trim();
+                //     console.log("Phone Number:", phoneNumber);
+                
+                //     let countryCode = document.querySelector("#country-code").value.trim();
+                //     console.log("Country Code:", countryCode);
+                
+                //     // Country code mapping
+                //     let countryCodeMap = {
+                //         "91": "IN",
+                //         "971": "AE"
+                //     };
+                
+                //     // Extract country abbreviation using the map
+                //     let country = countryCodeMap[countryCode] || "";
+                //     console.log("Country:", country);
+                
+                //     // Combine country code and phone number (without + and space)
+                //     let mobilenumber_code = `${countryCode}${phoneNumber}`;
+                //     console.log("Full Mobile Number:", mobilenumber_code);
+                
+                //     // Send event to Salesforce
+                //     SalesforceInteractions.sendEvent({
+                //         interaction: {
+                //             name: "Login",
+                //         },
+                //         user: {
+                //             attributes: {
+                //                 phoneNumber: mobilenumber_code,  // Example: 919876543210
+                //                 eventType: 'contactPointPhone',
+                //                 sourceLocale: country,  // Example: IN
+                //             },
+                //         },
+                //     });
+                    
+                // }),
+
+
+                listener("click", ".continueBtn", (event) => {
+                    console.log("Entered login with password");
+                    event.preventDefault(); // Prevent default action
+                
                     let phoneNumber = document.querySelector("#phone").value.trim();
                     console.log("Phone Number:", phoneNumber);
                 
@@ -498,11 +540,8 @@ document.addEventListener("DOMContentLoaded", function () {
                             },
                         },
                     });
-                    
                 }),
-
-
-
+                
 
                 listener("click", ".heroSection_prdrankbtn__oLW5s", (event) => {
                     console.log("Predict rank button clicked");
